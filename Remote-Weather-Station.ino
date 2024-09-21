0bda2ef5a9a57e3c99074548fc7ca5d874ea467f
jamesnll <langillejames11@gmail.com>
2024-09-20 19:09:01 -0700

    Connected ESP32 to local WiFi network

--- Remote-Weather-Station.ino
+++ Remote-Weather-Station.ino
@@ -1,5 +1,29 @@
+#include <WiFi.h>
+#include "WiFi-Secrets.h"
+
 void setup()
 {
+  Serial.begin(115200); // Start the Serial Monitor
+
+  // Start the connection process
+  Serial.printf("Connecting to %s\n", SECRET_SSID);
+
+  // Initiate the connection
+  WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
+
+  // Wait until the connection is established
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println(".");
+  Serial.println("WiFi connected!");
+
+  // Print the ESP32's IP address
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
 }
 
